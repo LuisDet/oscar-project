c965a19da2c7749fa81da15c79c6425db02a29f1
Luisdt <luisedenist@gmail.com>
2025-03-05 01:51:25 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://LuisDet.github.io',
+    base: 'oscar-project',
+    output: 'static',
+    root: './wide-turn-app',
+});
